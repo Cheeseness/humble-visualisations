328f9645a9e61db4f8d44d603835a2c8ec0a59ea
Cheeseness (Josh Bush) <contact@jbushproductions.com>
2013-11-22 17:59:55 +1000

    Made mobile 3 the default hourly bundle displayed.

--- ts/index.php
+++ ts/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns='http://www.w3.org/1999/xhtml' lang='en'>
 <?php
-	$title = "warner";
+	$title = "mobile 3";
 	if (isset($_GET['bundle']))
 	{
 		$title = urldecode($_GET['bundle']);
