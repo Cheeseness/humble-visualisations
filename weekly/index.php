733e6837f1c8dc7083b39e6ee6891ddcc6163487
Cheeseness (Josh Bush) <contact@jbushproductions.com>
2013-12-20 15:59:12 +1000

    Added Android 8 bundle and Puppy Games weekly sale as hourly defaults.

--- weekly/index.php
+++ weekly/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns='http://www.w3.org/1999/xhtml' lang='en'>
 <?php
-	$title = "weekly sale";
+	$title = "Puppy Games";
 	if (isset($_GET['bundle']))
 	{
 		$title = urldecode($_GET['bundle']);
